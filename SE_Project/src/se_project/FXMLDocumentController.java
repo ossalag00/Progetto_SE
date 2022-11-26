fe9247169f7c2db64eb4652abf839bdbf2e7d155
cirociara <117932951+cirociara@users.noreply.github.com>
2022-11-26 19:09:49 +0100

    controller done

--- SE_Project/src/se_project/FXMLDocumentController.java
+++ SE_Project/src/se_project/FXMLDocumentController.java
@@ -4,11 +4,16 @@
  */
 package se_project;
 
+import Tool.EllipseTool;
+import Tool.LineTool;
+import Tool.RectangleTool;
+import Tool.ToolBar;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
@@ -16,11 +21,9 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TitledPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
-/**
- *
- * @author guari
- */
+
 public class FXMLDocumentController implements Initializable {
     
     private Label label;
@@ -41,46 +44,71 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Pane DrawingWindow;
     
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+
+    ToolBar toolBar=new ToolBar();
+    RectangleTool recTool=new RectangleTool();
+    EllipseTool ellipseTool=new EllipseTool();
+    LineTool lineTool=new LineTool();
+    Invoker invoker= new Invoker();
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-
+    @FXML
+    private void initDraw(MouseEvent event) {
+        if(toolBar.getCurrentState()==null){
+            Alert a = new Alert(Alert.AlertType.WARNING,"You must select a shape");
+            a.show();
+        }
+        else{
+        toolBar.getCurrentState().setStartX(event.getX());
+        toolBar.getCurrentState().setStartY(event.getY());}
+        }/*Metodo richiamato al click del mouse sull'area di disegno che verifica se
+        sia stata selezionata una figura(State) e, in tal caso, richiama i metodi
+        del Tool selezionato per memorizzare le coordinate iniziali, in caso non sia
+        stata selezionata alcuna figura restituisce un avviso a schermo*/
+    @FXML
+    private void draw(MouseEvent event){
+        toolBar.getCurrentState().setEndX(event.getX());
+        toolBar.getCurrentState().setEndY(event.getY());
+        invoker.setCommand(new DrawCommand(toolBar.getCurrentState(),DrawingWindow));
+        invoker.executeCommand();   
+    }/*Metodo che al rilascio del mouse setta le coordinate di arrivo del tool
+    selezionato e tramite l'invoker richiama il comando per disegnare la figura*/
+    
     @FXML
     private void setRectangle(ActionEvent event) {
+        toolBar.setCurrentState(recTool);
     }
 
     @FXML
     private void setEllipse(ActionEvent event) {
+        toolBar.setCurrentState(ellipseTool);
     }
 
     @FXML
     private void setLine(ActionEvent event) {
+        toolBar.setCurrentState(lineTool);
     }
 
     @FXML
     private void pickColorOut(ActionEvent event) {
+        Color c=colorPickerOut.getValue();
+        recTool.setOut(c);
+        ellipseTool.setOut(c);
+        lineTool.setOut(c);
     }
 
     @FXML
     private void pickColorIn(ActionEvent event) {
+        Color c=colorPickerIn.getValue();
+        recTool.setIn(c);
+        ellipseTool.setIn(c);
     }
 
     @FXML
     private void clear(ActionEvent event) {
+        DrawingWindow.getChildren().clear();
     }
-
-    @FXML
-    private void draw(MouseEvent event) {
-    }
-
-    @FXML
-    private void initDraw(MouseEvent event) {
-    }
-    
 }
